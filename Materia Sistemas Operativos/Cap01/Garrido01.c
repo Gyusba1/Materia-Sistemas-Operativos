88acff0679a192bb50961aef296cac98812a7aad
Gyusba1 <134864153+Gyusba1@users.noreply.github.com>
2024-07-28 12:27:37 -0500

    Update Garrido01.c

--- Materia Sistemas Operativos/Cap01/Garrido01.c
+++ Materia Sistemas Operativos/Cap01/Garrido01.c
@@ -1,1 +1,46 @@
+/******************************************************
+Autor: Felipe Andrés Garrido Flores
+Fecha: 23/07/2024
+Profesor: J.Corredor
+Materias: Sistemas Operativos
+Tmea: Programación en C
+Requerimientos:
+          -Función que sume 2 enteros
+          -Función que calcule el área de una circunferencia
+          -Función de bienvenida
+************************************************************/
 
+#include <stdio.h>
+
+//Función bienvenida
+
+void bienvenida(){
+  printf("Hola Mundo...\n");
+  printf("\t\t Bienvenidos a mi clase de SISTEMAS OPERATIVOS \n");
+}
+
+//Función que sume 2 enteros
+
+int suma2enteros(){
+  printf("Primer entero: 2");
+  printf("Segundo entero: 4");
+  int suma = 2 + 4;
+  printf("Suma es: %d\n", suma);
+     
+}
+
+//Area de circunferencia
+
+void areacircunferencia(){
+  int radio = 2;
+  float pi = 3,14
+  printf("El radio es: %d\n", radio);
+  float area = radio*radio*pi;
+  printf("El radio es: %d\n", radio);
+}
+
+int main(int argc, char *argv[]){
+  
+
+  
+}
