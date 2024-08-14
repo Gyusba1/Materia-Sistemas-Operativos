1b2248401292a3295633a8dd51c3c8d1b36c8dc4
Gyusba1 <134864153+Gyusba1@users.noreply.github.com>
2024-08-13 22:18:35 -0500

    Update cemAlloc01_Garrido.c

--- Materia Sistemas Operativos/Cap01/Gestion de Memoria/cemAlloc01_Garrido.c
+++ Materia Sistemas Operativos/Cap01/Gestion de Memoria/cemAlloc01_Garrido.c
@@ -3,37 +3,34 @@
 * Autor: Felipe Andres Garrido
 * Materia: Sistemas Operativos
 * Tema: Gestión de memoria
-* Tópico: Reserva de memoria con la función malloc()
-* La función malloc reserva un espacio de memoria de tamaño requerido.
-* Los espacios son de igual tamaño y ed memoria contiggua
-* La asignación es dinámica y útil para complejas destructuras de datos
-* inicializados en 0
-* devuelve un puntero señalando el inicio del espacio de memoria reservado.
+* Tópico: Reserva de memoria con la función calloc()
+* La función calloc reserva un espacio de memoria de tamaño requerido.
+* Los espacios son de igual tamaño y de memoria contigua.
+* La asignación es dinámica y útil para estructuras de datos complejas.
+* Además, los bloques de memoria asignados se inicializan en 0.
+* Devuelve un puntero señalando el inicio del espacio de memoria reservado.
 *************************************************************************************************/
+
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-  // Declarar variables y pnteros
-  int *p1 = calloc(4, sizeof(int)); //Asignacioón de vector de 4 tamaño
-  int *p2 = calloc(10, sizeof(int[4])); 
-  int *p3 = calloc(5, sizeof *p3); //bloque de memoria de 5
+int main() {
+    // Declarar variables y punteros
+    int *p1 = calloc(4, sizeof(int)); // Asignación de vector de tamaño 4
+    int (*p2)[4] = calloc(10, sizeof(int[4])); 
+    int *p3 = calloc(5, sizeof(*p3)); // Bloque de memoria de 5 enteros
 
-  // Llenar espacios primero bloque reservado (puntero)
 
-  printf("Construcción y calculo de la secuencia de loa primeros 10 términos\n");
-  for(int i=0; i<10; i++){
-    
-    *(p2+i) = i; 
-    printf("El valor 9na posicion de puntero es %d \n\n", *(p2 + 8));
-    
-  }
+    for (int i = 0; i < 10; i++) {
+        p2[i][0] = i; // Asignar el valor en la primera columna de cada fila
+        printf("El valor en la 1ra columna de la fila %d es %d\n", i, p2[i][0]); // Imprimir el valor en la columna 1 de la fila actual
+    }
 
-  // LIBERACION DE MEMORIA
-  free(p1);
-  free(p2);
-  free(p3);
+    // LIBERACIÓN DE MEMORIA
+    free(p1);
+    free(p2);
+    free(p3);
 
-  return 0;
+    return 0;
 }
