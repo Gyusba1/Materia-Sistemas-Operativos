693d95d37e420efa8706e29d952ed9acd514ce91
Gyusba1 <134864153+Gyusba1@users.noreply.github.com>
2024-08-22 20:59:20 -0500

    Update reAlloc01_Garrido.c

--- Materia Sistemas Operativos/Cap01/Gestion de Memoria/reAlloc01_Garrido.c
+++ Materia Sistemas Operativos/Cap01/Gestion de Memoria/reAlloc01_Garrido.c
@@ -3,7 +3,7 @@
 * Autor: Felipe Andres Garrido
 * Materia: Sistemas Operativos
 * Tema: Gestión de memoria
-* Tópico: Función realloc(), primero debemos crear ya sea un cemalloc() o un memalloc() ya que realloc() 
+* Tópico: Función realloc(), primero debemos crear ya sea un calloc() o un malloc() ya que realloc() 
 lo que hace es como actualizar el espacio de memoria reservado, si realloc() actualiza a un tamaño mayor
 se añade memoria al final, pero si es menor, los datos se perderán.
 ******************************************************************/
